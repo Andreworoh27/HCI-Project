510de78181a41958f691202dd534bac418515626
Andrew <andreworoh.ao@gmail.com>
2022-06-06 13:27:34 +0800

    Create member page

---
+++ Member/script.js
@@ -0,0 +1,17 @@
+// let Name = document.getElementById("#Name").innerHTML;
+// let DOB = document.getElementById("#DOB").innerHTML;
+// let Gender = document.getElementById("#Gender").innerHTML;
+// let Address = document.getElementById("#Address").innerHTML;
+// let Phone = document.getElementById("#Phone").innerHTML;
+// let Email = document.getElementById("#Email").innerHTML;
+// let Password = document.getElementById("#Password").innerHTML;
+// let Dailyletter = document.getElementById("#Dailyletter").innerHTML;
+
+// console.log(Name)
+// console.log(DOB)
+// console.log(Gender)
+// console.log(Address)
+// console.log(Phone)
+// console.log(Email)
+// console.log(Password)
+// console.log(Dailyletter)
