1d85d7b9d85aefa4f15e13fe1e882cb3746ba622
Andrew <andreworoh.ao@gmail.com>
2022-06-09 17:59:03 +0800

    add member page : footer, media query for phone, and js validation
    - validation doesn't work (fix required)
    - image add for main page

--- Member/script.js
+++ Member/script.js
@@ -15,3 +15,34 @@
 // console.log(Email)
 // console.log(Password)
 // console.log(Dailyletter)
+
+function validation() {
+  const form = document.forms["member"];
+  const Name = form["Name"].value;
+  const DOB = form["DOB"].value;
+  const Gender = form["Gender"].value;
+  const Address = form["Address"];
+  const Phone = form["Phone"];
+  const Email = form["Email"].value;
+  const Password = form["Password"];
+  const Dailyletter = form["DailyLetter"].value;
+  document.getElementById("error").innerHTML = kosong;
+  let name = Name;
+  document.getElementById("nametext").innerHTML = name;
+  return false;
+
+  if (Gender != "Male" || Gender != "Female") {
+    document.getElementById("genderalert").innerHTML = "choose your gender";
+    return false;
+  }
+
+  if (!Email.endsWith("@gmail.com") || !Email.endsWith("@binus.ac.id") || !Email.endsWith("@yahoo.com")) {
+    document.getElementById("emailalert").innerHTML = "Wrong email format";
+    return false;
+  }
+
+  if (Password.length() < 8) {
+    document.getElementById("passwordalert").innerHTML = "8 characters minimal";
+    return false;
+  }
+}
