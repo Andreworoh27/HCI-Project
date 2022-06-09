6f04c36c4043bc528337e65e7a63b68ae9b46e12
Andrew <andreworoh.ao@gmail.com>
2022-06-09 18:00:07 +0800

    add galery page : footer, and media query for phone
    - tablet media query (fix required coz of overflow x)

--- Gallery/scriptgallery.js
+++ Gallery/scriptgallery.js
@@ -11,50 +11,50 @@ const button10 = document.getElementById("tombol-10");
 const button11 = document.getElementById("tombol-11");
 const button12 = document.getElementById("tombol-12");
 
-button1.addEventListener("click", function(){
-    alert("DY/DX has been addedd to cart!")
+button1.addEventListener("click", function () {
+  alert("DY/DX has been addedd to cart!");
 });
 
-button2.addEventListener("click", function(){
-    alert("Apex has been addedd to cart!")
+button2.addEventListener("click", function () {
+  alert("Apex has been addedd to cart!");
 });
 
-button3.addEventListener("click", function(){
-    alert("TransAcoustic has been addedd to cart!")
+button3.addEventListener("click", function () {
+  alert("TransAcoustic has been addedd to cart!");
 });
 
-button4.addEventListener("click", function(){
-    alert("Classica has been addedd to cart!")
+button4.addEventListener("click", function () {
+  alert("Classica has been addedd to cart!");
 });
 
-button5.addEventListener("click", function(){
-    alert("GCR has been addedd to cart!")
+button5.addEventListener("click", function () {
+  alert("GCR has been addedd to cart!");
 });
 
-button6.addEventListener("click", function(){
-    alert("MX Series has been addedd to cart!")
+button6.addEventListener("click", function () {
+  alert("MX Series has been addedd to cart!");
 });
 
-button7.addEventListener("click", function(){
-    alert("Electrica has been addedd to cart!")
+button7.addEventListener("click", function () {
+  alert("Electrica has been addedd to cart!");
 });
 
-button8.addEventListener("click", function(){
-    alert("AllStar has been addedd to cart!")
+button8.addEventListener("click", function () {
+  alert("AllStar has been addedd to cart!");
 });
 
-button9.addEventListener("click", function(){
-    alert("Atlantica has been addedd to cart!")
+button9.addEventListener("click", function () {
+  alert("Atlantica has been addedd to cart!");
 });
 
-button10.addEventListener("click", function(){
-    alert("R301 has been addedd to cart!")
+button10.addEventListener("click", function () {
+  alert("R301 has been addedd to cart!");
 });
 
-button11.addEventListener("click", function(){
-    alert("Attitude Limit 3 has been addedd to cart!")
+button11.addEventListener("click", function () {
+  alert("Attitude Limit 3 has been addedd to cart!");
 });
 
-button12.addEventListener("click", function(){
-    alert("BPJS2 has been addedd to cart!")
+button12.addEventListener("click", function () {
+  alert("BPJS2 has been addedd to cart!");
 });
